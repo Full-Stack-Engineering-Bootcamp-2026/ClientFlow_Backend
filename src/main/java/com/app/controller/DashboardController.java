235635df06dc72370a9b9a916006dff1920223af
Rishita Pathak <rishita.pathak@mindbowser.com>
2026-05-10 16:42:15 +0530

    Admin code merged2

--- src/main/java/com/app/controller/DashboardController.java
+++ src/main/java/com/app/controller/DashboardController.java
@@ -1,11 +1,14 @@
 package com.app.controller;
 
 import com.app.dto.DoctorScheduleDashboardResponse;
+import com.app.dto.StaffResponse;
 import com.app.response.ApiResponse;
 import com.app.service.DashboardService;
+import com.app.service.StaffService;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.data.domain.Page;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -20,6 +23,8 @@ import java.util.List;
 public class DashboardController {
 
     private final DashboardService dashboardService;
+
+    private final StaffService staffService;
 
     @GetMapping("/doctor-schedule")
     @PreAuthorize("hasRole('ADMIN')")
@@ -39,6 +44,30 @@ public class DashboardController {
                                 dashboardService
                                         .getDoctorScheduleDashboard(startDate)
                         )
+                        .build()
+        );
+    }
+
+    @GetMapping("/recent-staff")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ApiResponse<Page<StaffResponse>>> getRecentStaff(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size
+    ) {
+
+        Page<StaffResponse> data = staffService.getAllStaff(
+                page,
+                size,
+                null,
+                null,
+                null
+        );
+
+        return ResponseEntity.ok(
+                ApiResponse.<Page<StaffResponse>>builder()
+                        .success(true)
+                        .message("Recent staff fetched successfully")
+                        .data(data)
                         .build()
         );
     }
